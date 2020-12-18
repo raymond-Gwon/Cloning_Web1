3a09af9708967fe664d812be68fdd71722d2b48c
Raymond <kks900027@gmail.com>
2020-12-18 11:43:28 +0900

    fixed slider

--- js/slider.js
+++ js/slider.js
@@ -2,23 +2,20 @@ const slider = document.querySelector('.slider');
 let slides = document.querySelectorAll('.slider .slider-content');
 let counter = 0;
 
-function moveToNext () {
-    counter++;
-    let move = -15 * counter;
-    let space = -10 * counter;
-    slider.style.transform = `translate(calc(${move}vw + ${space}px))`;
-}
-function moveToPrev () {
-    counter--;
-    let move = -15 * counter;
-    let space = -10 * counter;
-    slider.style.transform = `translate(calc(${move}vw + ${space}px))`;
-}
 
-
-slider.addEventListener('transitionend', function(){
-    console.log(slides[counter].id)
-    if (slides[counter].id === 'lastClone'){
-        console.log('done');
+document.querySelector('.next-btn').addEventListener('click', function(){
+    if (counter<slides.length-4){
+        counter++;
+        let move = -15 * counter;
+        let space = -10 * counter;
+        slider.style.transform = `translate(calc(${move}vw + ${space}px))`;
     }
-})
+});
+document.querySelector('.pre-btn').addEventListener('click', function(){
+    if(counter>0) {
+        counter--;
+        let move = -15 * counter;
+        let space = -10 * counter;
+        slider.style.transform = `translate(calc(${move}vw + ${space}px))`;
+    }
+});
